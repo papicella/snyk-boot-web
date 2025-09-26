937ccfcba7f00edf52e7ebf5240d9925fd395273
papicella <pas.apicella@gmail.com>
2025-09-26 16:51:07 +1000

    Add Rest controller

---
+++ src/main/java/com/example/snykbootweb/HelloRest.java
@@ -0,0 +1,15 @@
+package com.example.snykbootweb;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/hello")
+@CrossOrigin("*")
+public class HelloRest {
+
+    @GetMapping ("/sayhello")
+    public String sayHello() {
+        return "hello World!!!";
+    }
+}
