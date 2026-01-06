fce6e9681fac17c4590d73cd5d78018eae4adb3a
दीप्त शर्मा <deeptsharma@live.in>
2026-01-06 16:36:17 +0530

    Test snyk

--- src/main/java/com/example/snykbootweb/SnykBootWebApplication.java
+++ src/main/java/com/example/snykbootweb/SnykBootWebApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SnykBootWebApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("Hello world");
         SpringApplication.run(SnykBootWebApplication.class, args);
     }
 
